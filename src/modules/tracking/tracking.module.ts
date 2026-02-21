c233168c4eecd75a32bca50fb97863524d80fa26
JILZXY <Sasosiso79@gmail.com>
2026-02-20 18:50:39 -0600

    feat: se implemento el module del tracking

---
+++ src/modules/tracking/tracking.module.ts
@@ -0,0 +1,20 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { TrackingController } from './tracking.controller';
+import { DailyLogRepository } from './infrastructure/repositories/daily-log.repository';
+import { CreateLogUseCase } from './application/create-log.usecase';
+import { GetLogHistoryUseCase } from './application/get-log-history.usecase';
+import { GetStatisticsUseCase } from './application/get-statistics.usecase';
+import { StreakModule } from '../streak/streak.module';
+
+@Module({
+  imports: [forwardRef(() => StreakModule)], // forwardRef por dependencia circular con streak
+  controllers: [TrackingController],
+  providers: [
+    DailyLogRepository,
+    CreateLogUseCase,
+    GetLogHistoryUseCase,
+    GetStatisticsUseCase,
+  ],
+  exports: [DailyLogRepository],
+})
+export class TrackingModule {}
