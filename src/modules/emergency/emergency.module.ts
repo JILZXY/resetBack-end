df0da222e6cbbe061e7503bd079ec8e0ad999943
JILZXY <Sasosiso79@gmail.com>
2026-02-27 01:09:53 -0600

    feat: module de emergencia

---
+++ src/modules/emergency/emergency.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { EmergencyController } from './emergency.controller';
+import { ContactRepository } from './infrastructure/repositories/contact.repository';
+import { AlertRepository } from './infrastructure/repositories/alert.repository';
+import { NotificationService } from './infrastructure/services/notification.service';
+import { AddContactUseCase } from './application/add-contact.usecase';
+import { TriggerAlertUseCase } from './application/trigger-alert.usecase';
+import { NotifyContactsUseCase } from './application/notify-contacts.usecase';
+
+@Module({
+  controllers: [EmergencyController],
+  providers: [
+    ContactRepository,
+    AlertRepository,
+    NotificationService,
+    AddContactUseCase,
+    TriggerAlertUseCase,
+    NotifyContactsUseCase,
+  ],
+  exports: [AlertRepository],
+})
+export class EmergencyModule {}
