4adabe5160cd7d3f87b019c0f203c784c47b1a3b
JILZXY <Sasosiso79@gmail.com>
2026-03-02 09:14:00 -0600

    feat: cree el dto para actualizar post

---
+++ src/modules/forum/infrastructure/dtos/update-post.dto.ts
@@ -0,0 +1,22 @@
+import { IsArray, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class UpdatePostDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  tags?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  images?: string[];
+}
