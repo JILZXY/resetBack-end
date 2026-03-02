d5874b3df1aad43ffc5b3a81ffc407d4e90d80a9
JILZXY <Sasosiso79@gmail.com>
2026-03-02 09:09:08 -0600

    feat: cree el esquema del foro maneajdo con mongodb

---
+++ src/modules/forum/schemas/post.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PostDocument = HydratedDocument<Post>;
+
+@Schema({ collection: 'posts', timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+export class Post {
+  @Prop({ required: true })
+  authorId: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ required: true, default: false })
+  isAnonymous: boolean;
+
+  @Prop({ type: [String], default: [] })
+  images: string[];
+
+  @Prop({ type: [String], default: [] })
+  tags: string[];
+
+  @Prop({ default: 0 })
+  reactionUps: number;
+
+  @Prop({ default: 0 })
+  commentCount: number;
+}
+
+export const PostSchema = SchemaFactory.createForClass(Post);
