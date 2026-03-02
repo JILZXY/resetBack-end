377dff05d4bb5253ae54bfab74c930382e67ee04
JILZXY <Sasosiso79@gmail.com>
2026-03-02 09:14:49 -0600

    feat: cree el dto para crea comentarios

---
+++ src/modules/forum/infrastructure/dtos/create-comment.dto.ts
@@ -0,0 +1,14 @@
+import { IsBoolean, IsMongoId, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @IsBoolean()
+  is_anonymous: boolean;
+
+  @IsOptional()
+  @IsMongoId({ message: 'parent_id debe ser un ObjectId válido' })
+  parent_id?: string;
+}
