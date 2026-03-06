0aadb8b510143b74d674bb9d69c27e6d66ff7135
Fullgamer494 <fullgamer494@hotmail.com>
2026-03-06 12:13:13 -0600

    refactor: permitir conexiones CORS desde puerto local:3000

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,16 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
 
+  const frontendUrls = configService.get<string>('app.frontendUrl');
+  const origins = frontendUrls ? frontendUrls.split(',') : '*';
+
   app.enableCors({
-    origin: configService.get<string>('app.frontendUrl'), // Ajustar en producción con los dominios del front
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    origin: origins,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
   });
 
   const port = configService.get<number>('app.port') ?? 3000;
   await app.listen(port);
-  console.log(`🚀 Reset API corriendo en: http://localhost:${port}/api/v1`);
 }
 bootstrap();
