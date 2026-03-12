8e471e9974a2bd49cea2b71ae63a8d58e36834b1
Fullgamer494 <fullgamer494@hotmail.com>
2026-03-12 12:28:46 -0600

    fix: botón de emergencia

--- src/modules/emergency/infrastructure/dtos/trigger-alert.dto.ts
+++ src/modules/emergency/infrastructure/dtos/trigger-alert.dto.ts
@@ -1,4 +1,4 @@
-import { IsBoolean, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString, IsBoolean } from 'class-validator';
 
 export class TriggerAlertDto {
   @IsOptional()
