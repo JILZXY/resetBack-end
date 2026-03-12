078ebb2521e70022926107fba05a7cf451d9372b
Fullgamer494 <fullgamer494@hotmail.com>
2026-03-12 05:10:23 -0600

    chore: eliminación de configuraciones obsoletas de sendgrid y prisma

--- src/config/sendgrid.config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export const sendgridConfig = registerAs('sendgrid', () => ({
-  apiKey: process.env.SENDGRID_API_KEY,
-  fromEmail: process.env.SENDGRID_FROM_EMAIL,
-  fromName: process.env.SENDGRID_FROM_NAME,
-}));
